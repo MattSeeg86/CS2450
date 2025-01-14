3e0503165003cbe100b7ef3c8724ed95f0340326
matthew.seegmiller <matthew.seegmiller@utahtech.edu>
2025-01-14 15:54:22 -0700

    added space before guess

--- personal/guessing game.py
+++ personal/guessing game.py
@@ -7,10 +7,10 @@ while(guessed == False):
     guess = random.randint(15,30)
     if guess in guesses:
         continue
-    response = input("Are you"+str(guess) + " years old?")
+    response = input("Are you "+str(guess) + " years old?")
     if response == "yes":
         guessed = True
-        print("I guessed it! You're" + str(guess) + " years old")
+        print("I guessed it! You're " + str(guess) + " years old")
     if response == "no":
         print("I'll try again")
         guesses.append(guess)
