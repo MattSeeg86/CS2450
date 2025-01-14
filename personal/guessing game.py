f18199f7a8e20439834e873a7aa79cc6b905db05
matthew.seegmiller <matthew.seegmiller@utahtech.edu>
2025-01-14 15:30:26 -0700

    Added list to not guess the same number

--- personal/guessing game.py
+++ personal/guessing game.py
@@ -1,12 +1,16 @@
 import random
 print("Hi! I'm going to try to guess your age")
 name = input("What's your name?")
+guesses = []
 guessed = False
 while(guessed == False):
     guess = random.randint(15,30)
-    response = input("Are you"+str(guess) + "years old?")
+    if guess in guesses:
+        continue
+    response = input("Are you"+str(guess) + " years old?")
     if response == "yes":
         guessed = True
-        print("I guessed it! You're" + str(guess) + "years old")
+        print("I guessed it! You're" + str(guess) + " years old")
     if response == "no":
         print("I'll try again")
+        guesses.append(guess)
